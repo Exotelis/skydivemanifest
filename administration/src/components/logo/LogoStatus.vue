f10145dc49eb1ed2719fa82ec16f4897598e8a0b
Sebastian Krah <exotelis@mailbox.org>
2020-02-03 23:16:21 +0100

    admin: initial commit
    Adds a basic Vue.js project set up with the Vue CLI.
    
    administration/.browserslistrc
    administration/.editorconfig
    administration/.env
    administration/.eslintrc.js
    administration/.gitignore
    administration/README.md
    administration/babel.config.js
    administration/jest.config.js
    administration/package-lock.json
    administration/package.json
    administration/public/.htaccess
    administration/public/favicon.ico
    administration/public/favicon.png
    administration/public/img/icons/android-chrome-192x192.png
    administration/public/img/icons/android-chrome-512x512.png
    administration/public/img/icons/apple-touch-icon-120x120.png
    administration/public/img/icons/apple-touch-icon-152x152.png
    administration/public/img/icons/apple-touch-icon-180x180.png
    administration/public/img/icons/apple-touch-icon-60x60.png
    administration/public/img/icons/apple-touch-icon-76x76.png
    administration/public/img/icons/apple-touch-icon.png
    administration/public/img/icons/favicon-16x16.png
    administration/public/img/icons/favicon-32x32.png
    administration/public/img/icons/msapplication-icon-144x144.png
    administration/public/img/icons/mstile-150x150.png
    administration/public/img/icons/safari-pinned-tab.svg
    administration/public/index.html
    administration/public/robots.txt
    administration/src/App.vue
    administration/src/assets/images/login-wingsuit.jpg
    administration/src/assets/images/login.jpg
    administration/src/assets/images/login2.jpg
    administration/src/assets/images/logo.png
    administration/src/assets/images/sources.txt
    administration/src/assets/scss/_general.scss
    administration/src/assets/scss/_login.scss
    administration/src/assets/scss/_mdi.scss
    administration/src/assets/scss/_overwrites.scss
    administration/src/assets/scss/_theme.scss
    administration/src/assets/scss/app.scss
    administration/src/assets/scss/themes/default/_varibales.scss
    administration/src/assets/scss/themes/default/components/_logostatus.scss
    administration/src/assets/scss/themes/default/components/_mainnavigation.scss
    administration/src/assets/scss/themes/default/layouts/_default.scss
    administration/src/assets/scss/themes/default/layouts/_welcome.scss
    administration/src/components/HelloWorld.vue
    administration/src/components/layouts/BlankLayout.vue
    administration/src/components/layouts/DefaultLayout.vue
    administration/src/components/layouts/WelcomeLayout.vue
    administration/src/components/logo/LogoStatus.vue
    administration/src/components/pages/DashboardPage.vue
    administration/src/components/pages/LoginPage.vue
    administration/src/components/pages/SettingsPage.vue
    administration/src/components/pages/__tests__/LoginPage.spec.js
    administration/src/components/sm-login/sm-login.vuei
    administration/src/components/ui/LanguageSelector.vuei
    administration/src/components/ui/MainNavigation.vue
    administration/src/components/ui/__tests__/LanguageSelector.spec.js
    administration/src/components/ui/__tests__/LogoLicenseStatus.spec.js
    administration/src/i18n.js
    administration/src/locales/de.json
    administration/src/locales/en.json
    administration/src/locales/locales.json
    administration/src/main.js
    administration/src/registerServiceWorker.js
    administration/src/router/__tests__/router.spec.js
    administration/src/router/index.js
    administration/src/router/routes.js
    administration/src/views/About.vue
    administration/src/views/Home.vue
    administration/tests/unit/App.spec.ts
    administration/tests/unit/example.spec.js
    administration/tests/unit/i18n.spec.js
    administration/vue.config.js
    
    Signed-off-by: Sebastian Krah <exotelis@mailbox.org>

---
+++ administration/src/components/logo/LogoStatus.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <img src="@/assets/images/logo.png" class="logo" alt="Skydivemanifest Logo">
+    <div class="status-cirle"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LogoStatus'
+};
+</script>
