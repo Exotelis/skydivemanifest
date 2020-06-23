1d899c5237a53104d3892741f77f7d8e33681d8c
Sebastian Krah <exotelis@mailbox.org>
2020-06-23 23:17:48 +0200

    admin: add - Router tests
    Adds test for authentication and password change routing.

--- administration/src/router/__tests__/router.spec.ts
+++ administration/src/router/__tests__/router.spec.ts
@@ -1,5 +1,6 @@
 import VueRouter from 'vue-router';
 import router from '../router';
+import AuthService from '@/services/AuthService';
 
 describe('router', () => {
   it('router is instance of VueRouter', () => {
@@ -10,6 +11,18 @@ describe('router', () => {
     router.push('/login');
     expect(router.currentRoute.path).toBe('/login');
     expect(document.title).toContain('Login');
+  });
+
+  it('navigate to "/" and change title', () => {
+    const spy = jest.spyOn(AuthService, 'checkAuth').mockImplementation(() => true);
+
+    router.push('/').catch(error => error);
+    router.onReady(() => {
+      expect(router.currentRoute.path).toBe('/');
+      expect(document.title).toContain('Dashboard');
+    });
+
+    spy.mockRestore();
   });
 
   it('navigate to "/settings" but redirect to "/login"', () => {
@@ -27,5 +40,18 @@ describe('router', () => {
     router.push('/notitle');
     expect(router.currentRoute.path).toBe('/notitle');
     expect(document.title).toBe(process.env.VUE_APP_TITLE);
+  });
+
+  it('navigate to password-change', () => {
+    const spyAuth = jest.spyOn(AuthService, 'checkAuth').mockImplementation(() => true);
+    const spyPassword = jest.spyOn(AuthService, 'passwordChangeRequired').mockImplementation(() => true);
+    router.push('/').catch(error => error);
+    router.onReady(() => {
+      expect(router.currentRoute.path).toBe('/password-change');
+      expect(document.title).toContain('Change password');
+    });
+
+    spyAuth.mockRestore();
+    spyPassword.mockRestore();
   });
 });
