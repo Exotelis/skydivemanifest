dbbf50c75c516ebc23d9507df981f77ff4ad60e8
Sebastian Krah <exotelis@mailbox.org>
2020-07-30 21:21:59 +0200

    admin: fix - api
    Adds XSRF-TOKEN as default header to api call.

--- administration/src/api.ts
+++ administration/src/api.ts
@@ -1,10 +1,12 @@
 import axios from 'axios';
 import { defaultLanguage } from '@/i18n';
+import { getCookie } from '@/helpers';
 
 // Axios default settings
 const credentials: boolean = process.env.VUE_APP_API_CREDENTIALS === 'true';
 axios.defaults.baseURL = process.env.VUE_APP_API_ENDPOINT || '';
 axios.defaults.headers.common['Content-Language'] = defaultLanguage;
 axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+axios.defaults.headers.common['X-XSRF-TOKEN'] = getCookie('XSRF-TOKEN');
 axios.defaults.headers.post['Content-Type'] = 'application/json';
 axios.defaults.withCredentials = credentials;
